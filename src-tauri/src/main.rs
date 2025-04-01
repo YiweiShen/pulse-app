55ebf2d0e7973b177247ffc6b6797cec2bdab402
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2025-04-01 01:04:09 -0400

    feat: integrate stronghold plugin for secure password handling

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,12 +12,37 @@ mod workspace_listener;
 
 use std::error::Error;
 use tauri::{App, AppHandle};
-
 /// Main entry point of the Tauri application.
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
+        // Set up stronghold
+        // https://tauri.app/plugin/stronghold/
+        .plugin(
+            tauri_plugin_stronghold::Builder::new(|password| {
+                // https://docs.rs/rust-argon2/latest/argon2/
+                use argon2::{hash_raw, Config, Variant, Version};
+
+                let config = Config {
+                    lanes: 4,
+                    mem_cost: 10_000,
+                    time_cost: 10,
+                    variant: Variant::Argon2id,
+                    version: Version::Version13,
+                    ..Default::default()
+                };
+
+                let salt = "bVoch2OKihFB2QL1XEaoqh17FAFysMpu".as_bytes();
+
+                let key =
+                    hash_raw(password.as_ref(), salt, &config).expect("failed to hash password");
+
+                key.to_vec()
+            })
+            .build(),
+        )
         .invoke_handler(tauri::generate_handler![
             command::init_menubar_panel,
             command::show_menubar_panel,
